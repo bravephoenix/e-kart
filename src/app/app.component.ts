1b48c2db2cba820cba73d50ba8dc7e511bd357a8
bravephoenix <sandeep.daiya45@gmail.com>
2018-05-27 03:42:33 +0530

    added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyD1v2WKVy_1iX3tA0NM5naoIiI8fcQs-Rc',
+      authDomain: 'e-kart-d733d.firebaseapp.com',
+      databaseURL: 'https://e-kart-d733d.firebaseio.com',
+      projectId: 'e-kart-d733d',
+      storageBucket: 'e-kart-d733d.appspot.com',
+      messagingSenderId: '252071520353'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
